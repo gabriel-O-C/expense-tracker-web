b488e1741713fa4e2c52c529a6debc301c639877
Gabriel de Oliveira Chaves <gabrieloliveirachaves754@gmail.com>
2021-10-14 10:22:17 -0300

    feat: date filter for current month

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-/* eslint-disable react/jsx-pascal-case */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { ThemeProvider } from 'styled-components';
 import * as C from './App.styles'
 import { Header } from './components';
 import theme from './styles/theme';
+import { items } from './data/items'
+import { getCurrentMonth } from './helpers/dateFilters'
+import { Item } from './types/Item';
 function App() {
+
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+  
+  useEffect(() => {
+    
+  }, [list, currentMonth])
   return (
     <ThemeProvider theme={theme}>
       <C.Container>
