85e539374cee9d532c22e46a20f04a071b8613d1
Gabriel de Oliveira Chaves <gabrieloliveirachaves754@gmail.com>
2021-10-25 16:01:30 -0300

    feat: more categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,8 +2,11 @@ import theme from "../styles/theme";
 import { Category } from '../types/Category'
 export const categories: Category = {
   food: { title: 'Alimentação', color: theme.colors.light_blue, expense: true},
-  salary: { title: 'bolsa estágio', color: theme.colors.green, expense: false },
+  salary: { title: 'salário', color: theme.colors.green, expense: false },
   internet: { title: 'conta de internet', color: theme.colors.purple, expense: true },
   water: { title: 'conta de água', color: theme.colors.blue, expense: true },
-  power: { title: 'conta de luz', color: theme.colors.yellow, expense: true }
+  power: { title: 'conta de luz', color: theme.colors.yellow, expense: true },
+  entertainment: { title: 'entretenimento', color: theme.colors.dark_gray, expense: true },
+  rent: { title: 'aluguel', color: theme.colors.blue, expense: true },
+  dividends: {title: 'dividendos', color: theme.colors.black, expense: false}
 }
